3965cc594e0b208720ee58ff57e9843d17d98552
Ma. Angelica S. Dichon <dichonmaangelica7@gmail.com>
2025-11-18 21:11:23 +0800

    delete functions

--- functions/src/index.js
+++
@@ -1,9 +0,0 @@
-import * as functions from 'firebase-functions';
-
-// You can add your functions here later
-// For now, this is just a placeholder to make deployment work
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
